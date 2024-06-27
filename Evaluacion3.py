67c9f15d4337a3e401391c7c755a499e2d74b17a
Anapan-bit <anaispalmasanchez@gmail.com>
2024-06-27 19:45:53 -0400

    Primer paso

---
+++ Evaluacion3.py
@@ -0,0 +1,15 @@
+import csv
+from datetime import datetime
+
+# Archivo donde se almacenarán los datos
+FILENAME = 'vehiculos.csv'
+
+# Función para mostrar el menú
+def mostrar_menu():
+    print("Menú:")
+    print("1. Guardar")
+    print("2. Buscar")
+    print("3. Listar")
+    print("4. Imprimir")
+    print("5. Salir")
+
